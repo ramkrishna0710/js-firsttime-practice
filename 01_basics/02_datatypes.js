00e0fee63aa95dcb38c84104563c51ac0b0231e5
Ramkrishna Mandal <113228237+ramkrishna0710@users.noreply.github.com>
2024-07-21 23:59:14 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";   // treat all JS code as newer version
+
+// alert(3+3)  // we are using nodejs, bot browser
+
+console.log("Ramkrishna");
+
+let name = "Ramkrishna"
+let age = 21
+let isValid = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null -> standalone value
+// undefined => 
+// symbol -> unique
+
+
+//object
+
+console.log(typeof "Ramkrishna");
+console.log(typeof age);
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
