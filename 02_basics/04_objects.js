a8e01c1405652519184038fdf3e5ffa577716608
Ramkrishna Mandal <113228237+ramkrishna0710@users.noreply.github.com>
2024-07-23 01:17:02 +0000

    object

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,11 +26,56 @@ const obj2 = {3: "c", 4: "d"}
 // const obj3 = Object.assign({}, obj1, obj2)
 
 const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+// console.log(obj3);
 
 const user = [
     {
         id:1,
         email: "r@gamil.com"
-    }
+    },
+    {
+        id:1,
+        email: "r@gamil.com"
+    },
+    {
+        id:1,
+        email: "r@gamil.com"
+    },
+    {
+        id:1,
+        email: "r@gamil.com"
+    },
+    {
+        id:1,
+        email: "r@gamil.com"
+    },
 ]
+
+
+user[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('age'));
+
+const course = {
+    courseName: "js in hindi",
+    price: "999",
+    courseInstuctor: "hitesh"
+}
+
+// course.courseInstuctor
+
+const {courseInstuctor : instructot} = course
+
+// console.log(courseInstuctor);
+console.log(instructot);
+
+// const navbar = (company) => {
+
+// }
+// navbar(company = 'hitesh')
+
