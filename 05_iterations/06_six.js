6f6391668c4c656fb01b4fab4100309496e5d43d
Ramkrishna Mandal <113228237+ramkrishna0710@users.noreply.github.com>
2024-07-24 06:28:39 +0000

    Filter map

---
+++ 05_iterations/06_six.js
@@ -0,0 +1,25 @@
+// const coding = ["js", "java", "cpp", "py", "ruby"]
+
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+//     return item
+// })
+// console.log(values);
+
+
+const myNUms = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// const newNums = myNUms.filter( (num) => num > 4)
+
+// const newNumsAgain = myNUms.filter( (num) => {
+//     return num > 4
+// })
+
+// const newNums = []
+// myNUms.forEach( (num) => {
+//     if(num>4) {
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums);
+
