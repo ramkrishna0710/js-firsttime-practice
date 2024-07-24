6d249c90bf2f90edc6420bff13e5f0f4699de43d
Ramkrishna Mandal <113228237+ramkrishna0710@users.noreply.github.com>
2024-07-24 06:10:29 +0000

    advance loop

---
+++ 05_iterations/04_four.js
@@ -0,0 +1,24 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'Swift by apple'
+}
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcut iss for ${myObject[key]}`)
+// }
+
+// const programming = ["js", "rb", "java", "cpp"]
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+const map = new Map()
+map.set('IN',"India")
+map.set('FR',"France")
+map.set('USA',"United States of America")
+
+// for (const key in map) {
+//     console.log(key);
+// }
