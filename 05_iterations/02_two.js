b949ad24adbe835acbe34947658c1aa60962d1f8
Ramkrishna Mandal <113228237+ramkrishna0710@users.noreply.github.com>
2024-07-24 05:36:41 +0000

    While and Do While

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,17 @@
+// WHILE LOOP
+
+// let i = 0
+// while ( i <= 10) {
+//     console.log(`Value of index is ${i}`);
+//     i += 2
+// }
+
+// DO-WHILE
+
+let score = 1
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
+
